cc731509e3614bb49c449eb62fd85c3c1ea312a5
Monte-Ntuli <86322309+Monte-Ntuli@users.noreply.github.com>
2026-01-12 20:52:29 +0200

    added favicon and other stuff

--- auto_update.bat
+++ auto_update.bat
@@ -7,6 +7,11 @@ echo ---------------------------------------------------
 SET "SOURCE=C:/Users/E7240/AppData/Roaming/MetaQuotes/Terminal/Common/Files/"
 SET "DEST=C:\Users\Public\Bean_Website\BEAN_TradeCloseLog.csv"
 SET "REPO=C:\Users\Public\Bean_Website"
+SET "SIG_SOURCE=C:\Users\E7240\AppData\Roaming\MetaQuotes\Terminal\Common\Files\Live_Signals.csv"
+SET "SIG_DEST=C:\Users\Public\Bean_Website\Live_Signals.csv"
+
+copy /Y "%SIG_SOURCE%" "%SIG_DEST%"
+copy /Y "C:\Users\E7240\AppData\Roaming\MetaQuotes\Terminal\Common\Files\Live_Signals.csv" "C:\Users\Public\Bean_Website\Live_Signals.csv"
 
 :: 2. Copy the latest log file to the website folder
 echo Copying latest trade logs...
